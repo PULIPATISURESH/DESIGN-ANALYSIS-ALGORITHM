7f0a63c0f36cacbd366d9865cc460cd2e97682eb
PULIPATISURESH <112693126+PULIPATISURESH@users.noreply.github.com>
2023-05-06 08:29:27 +0530

    Update binary search.c

--- binary search.c
+++ binary search.c
@@ -1,29 +1,45 @@
-#include <stdio.h>
-int main()
-{
-int i, low, high, mid, n, key, array[100];
-printf("Enter number of elementsn");
-scanf("%d",&n);
-printf("Enter %d integersn", n);
-for(i = 0; i < n; i++)
-scanf("%d",&array[i]);
-printf("Enter value to findn");
-scanf("%d", &key);
-low = 0;
-high = n - 1;
-mid = (low+high)/2;
-while (low <= high) {
-if(array[mid] < key)
-low = mid + 1;
-else if (array[mid] == key) {
-printf("%d found at location %d.n", key, mid+1);
-break;
+#include<stdio.h>
+int main(){
+	int a,i,r,lis[20],j,t,min=0,max,mid,f,pos=0,q=0;
+	printf("Enter the range:");
+	scanf("%d",&a);
+	printf("Enter the numbers:\n");
+	for(i=0;i<a;i++){
+		printf("Enter the %d element:",i+1);
+		scanf("%d",&lis[i]);
+	}
+	for(i=0;i<a;i++){
+		for(j=i;j<a;j++){
+			if(lis[i]>lis[j]){
+				t=lis[i];
+				lis[i]=lis[j];
+				lis[j]=t;
+			}
+		}
+	}
+printf("The sorted list is:");
+for(i=0;i<a;i++){
+	printf(" %d ",lis[i]);
 }
-else
-high = mid - 1;
-mid = (low + high)/2;
+max=a;
+printf("\nEnter the element to find:");
+scanf("%d",&f);
+while(pos==0 && q<=a){
+	mid=(min+max)/2;
+	if(lis[mid]==f){
+		pos=mid+1;
+	}
+	else if(lis[min]<f){
+		min=mid+1;
+	}
+	else{
+		max=mid-1;
+	}
+	q++;
 }
-if(low > high)
-printf("Not found! %d isn't present in the list.n", key);
-return 0;
+if(pos!=0){
+printf("The position of given element %d is:%d",f,pos);}
+else{
+	printf("U given wrong input");
+}
 }
